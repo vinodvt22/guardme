3f767840229b4c956e80ebf58dfe77634349dce9
Dennis <dennisohere@live.com>
2018-05-01 09:43:36 +0100

    added tickets api

---
+++ local/app/Http/Controllers/Api/MessageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Responsive\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\Tickets\Traits\MessageTrait;
+use Modules\Tickets\Repositories\TicketRepository;
+use Modules\Tickets\Repositories\TicketMessageRepository;
+
+class MessageController extends Controller
+{
+    use MessageTrait;
+
+    private $ticket;
+
+    private $ticketMessages;
+
+    public function __construct(
+        TicketRepository $ticket,
+        TicketMessageRepository $ticketMessages)
+    {
+        $this->ticket = $ticket;
+        $this->ticketMessages = $ticketMessages;
+    }
+
+    public function store(Request $request, $ticketId)
+    {
+        $errors = $this->messageStore($request, $ticketId);
+
+        return response()->json([
+            'status' => $errors ? 500 : 200,
+            'errors' => $errors,
+        ]);
+    }
+}
