40b8d70ef1f951ee31158324e41ed35d35d0c83e
adnan00 <atahirhashmi@gmail.com>
2018-05-11 15:00:35 +0500

    after merging with master

--- local/resources/views/jobs/find.blade.php
+++ local/resources/views/jobs/find.blade.php
@@ -16,6 +16,16 @@
         $('#cat_val').val(val);
     }
 
+     $(document).ready(function(){
+     //$('.content-data').hide(); 
+        $('.skeleton').show();
+
+     }); 
+
+    $(window).load(function(){
+        $('.content-data').show(); 
+        $('.skeleton').hide();
+    });
     
 </script>
 
@@ -188,8 +198,94 @@
                         </div>
                     </div>
                     <!-- recommended-ads -->
-                    <div class="col-sm-8 col-md-7">             
-                        <div class="section job-list-item">
+                    <div class="col-sm-8 col-md-7">  
+
+                     <div class="section job-list-item skeleton">
+
+                        <div class="featured-top clearfix">
+                                
+                                <div class="dropdown pull-right">
+                                    <div class="dropdown category-dropdown">
+                                        <h5>Sort by:</h5>                       
+                                        <a data-toggle="dropdown" href="#"><span class="change-text">Most Relevant</span><i class="fa fa-caret-square-o-down"></i></a>
+                                        <ul class="dropdown-menu category-change">
+                                            <li><a href="#">Most Relevant</a></li>
+                                            <li><a href="#">Most Popular</a></li>
+                                        </ul>                               
+                                    </div><!-- category-change -->      
+                                </div>                          
+                            </div>
+
+                        <div class="timeline-item">
+                            <div class="animated-background facebook">
+                              <div class="background-masker header-top"></div>
+                              <div class="background-masker header-left"></div>
+                              <div class="background-masker header-right"></div>
+                              <div class="background-masker header-bottom"></div>
+                              <div class="background-masker subheader-left"></div>
+                              <div class="background-masker subheader-right"></div>
+                              <div class="background-masker subheader-bottom"></div>
+                              <div class="background-masker content-top"></div>
+                              <div class="background-masker content-first-end"></div>
+                              <div class="background-masker content-second-line"></div>
+                              <div class="background-masker content-second-end"></div>
+                              <div class="background-masker content-third-line"></div>
+                              <div class="background-masker content-third-end"></div>
+                            </div>
+                        </div>
+                        <div class="timeline-item">
+                            <div class="animated-background facebook">
+                              <div class="background-masker header-top"></div>
+                              <div class="background-masker header-left"></div>
+                              <div class="background-masker header-right"></div>
+                              <div class="background-masker header-bottom"></div>
+                              <div class="background-masker subheader-left"></div>
+                              <div class="background-masker subheader-right"></div>
+                              <div class="background-masker subheader-bottom"></div>
+                              <div class="background-masker content-top"></div>
+                              <div class="background-masker content-first-end"></div>
+                              <div class="background-masker content-second-line"></div>
+                              <div class="background-masker content-second-end"></div>
+                              <div class="background-masker content-third-line"></div>
+                              <div class="background-masker content-third-end"></div>
+                            </div>
+                        </div>
+                        <div class="timeline-item">
+                            <div class="animated-background facebook">
+                              <div class="background-masker header-top"></div>
+                              <div class="background-masker header-left"></div>
+                              <div class="background-masker header-right"></div>
+                              <div class="background-masker header-bottom"></div>
+                              <div class="background-masker subheader-left"></div>
+                              <div class="background-masker subheader-right"></div>
+                              <div class="background-masker subheader-bottom"></div>
+                              <div class="background-masker content-top"></div>
+                              <div class="background-masker content-first-end"></div>
+                              <div class="background-masker content-second-line"></div>
+                              <div class="background-masker content-second-end"></div>
+                              <div class="background-masker content-third-line"></div>
+                              <div class="background-masker content-third-end"></div>
+                            </div>
+                        </div>
+                        <div class="timeline-item">
+                            <div class="animated-background facebook">
+                              <div class="background-masker header-top"></div>
+                              <div class="background-masker header-left"></div>
+                              <div class="background-masker header-right"></div>
+                              <div class="background-masker header-bottom"></div>
+                              <div class="background-masker subheader-left"></div>
+                              <div class="background-masker subheader-right"></div>
+                              <div class="background-masker subheader-bottom"></div>
+                              <div class="background-masker content-top"></div>
+                              <div class="background-masker content-first-end"></div>
+                              <div class="background-masker content-second-line"></div>
+                              <div class="background-masker content-second-end"></div>
+                              <div class="background-masker content-third-line"></div>
+                              <div class="background-masker content-third-end"></div>
+                            </div>
+                        </div>
+                    </div>             
+                        <div class="section job-list-item content-data" style="display:none"">
                             <div class="featured-top">
                                 
                                 <div class="dropdown pull-right">
@@ -247,6 +343,10 @@
     <div class="col-md-12 noservice" align="center">No job matching found!</div>
     
     <?php } ?>
+
+     <div class="text-center">
+                {{$joblist->links()}}
+            </div>
     </div>
 </div>
 
