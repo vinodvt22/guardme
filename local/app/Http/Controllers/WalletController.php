307a9cabe16cac90fe921f923e70ce5ef66706b5
maysoon dahdouh <maysoon.azzat@gmail.com>
2018-05-20 18:39:23 +0200

    conflect files

--- local/app/Http/Controllers/WalletController.php
+++ local/app/Http/Controllers/WalletController.php
@@ -10,7 +10,11 @@ use Auth;
 use Crypt;
 use Responsive\Transaction;
 use Responsive\User;
+use Responsive\Job;
+use Responsive\JobApplication;
 use URL;
+use Carbon\Carbon;
+use PDF;
 
 class WalletController extends Controller
 {
@@ -23,7 +27,17 @@ class WalletController extends Controller
 	public function show() {
 		$wallet = new Transaction();
 		$wallet_data = $wallet->getAllTransactionsAndEscrowBalance();
-		return view('wallet', compact('wallet_data'));
+		// return view('wallet', compact('wallet_data'));
+		$user = auth()->user();
+		if($user->admin == 0){
+			$jobs = DB::select('select distinct security_jobs.id, security_jobs.title, sum(transactions.amount) as amount, security_jobs.created_at from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and transactions.user_id = '.$user->id.' group by job_id');
+		}else if($user->admin == 2){
+			$jobs = DB::select('select distinct security_jobs.id, security_jobs.title, sum(transactions.amount) as amount, security_jobs.number_of_freelancers, security_jobs.created_at from security_jobs, job_applications, transactions where job_applications.job_id = security_jobs.id and transactions.job_id = security_jobs.id and is_hired = 1 and applied_by = '.$user->id.' and transactions.type = "job_fee" group by security_jobs.id');
+		}else{
+            $jobs = array();
+        }
+		// dd($jobs);
+		return view('wallet', compact('jobs', 'wallet_data'));
 	}
 
 	public function view() {
@@ -31,6 +45,122 @@ class WalletController extends Controller
 		$wallet_data = $wallet->getAllTransactionsAndEscrowBalance();
 		$userid = auth()->user()->id;
 		$editprofile = User::where('id',$userid)->get();
-		return view('wallet-dashboard', compact('wallet_data', 'editprofile'));
+
+        $user = auth()->user();
+        if($user->admin == 0){
+            $jobs = DB::select('select distinct security_jobs.id, security_jobs.title, sum(transactions.amount) as amount, security_jobs.created_at from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and transactions.user_id = '.$user->id.' group by job_id');
+        }else if($user->admin == 2){
+            $jobs = DB::select('select distinct security_jobs.id, security_jobs.title, sum(transactions.amount) as amount, security_jobs.number_of_freelancers, security_jobs.created_at from security_jobs, job_applications, transactions where job_applications.job_id = security_jobs.id and transactions.job_id = security_jobs.id and is_hired = 1 and applied_by = '.$user->id.' and transactions.type = "job_fee" group by security_jobs.id');
+        }else{
+            $jobs = array();
+        }
+		return view('wallet-dashboard', compact('wallet_data', 'editprofile', 'jobs'));
 	}
+
+	public function searchJobs(Request $request) 
+    {
+    	$wallet = new Transaction();
+		$wallet_data = $wallet->getAllTransactionsAndEscrowBalance();
+    	$keyword = $request->keyword;
+    	$start_date = $request->start_date;
+    	$end_date = $request->end_date;
+    	$user = auth()->user();
+
+    	if($keyword != ''){
+			if($user->admin == 0){
+				$jobs = DB::select('select distinct security_jobs.id, security_jobs.title, transactions.amount, transactions.created_at from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and transactions.user_id = '.$user->id.' and (security_jobs.id like "%'.$keyword.'%" or security_jobs.title like "%'.$keyword.'%") group by job_id');
+			}else if($user->admin == 2){
+				$jobs = DB::select('select distinct security_jobs.id, security_jobs.title, transactions.amount, transactions.created_at from security_jobs, job_applications, transactions where job_applications.job_id = security_jobs.id and transactions.job_id = security_jobs.id and is_hired = 1 and applied_by = '.$user->id.' and (security_jobs.id like "%'.$keyword.'%" or security_jobs.title like "%'.$keyword.'%") group by security_jobs.id');
+			}
+    	}
+    	else if($start_date != null && $end_date != null && $start_date < $end_date){
+    		$format = "y_m_d";
+			$date1  = date("Y-m-d", strtotime($start_date));
+			$date2  = date("Y-m-d", strtotime($end_date));
+			// dd($date1." ".$date2);
+    		if($user->admin == 0){
+				$jobs = DB::select('select distinct security_jobs.id, security_jobs.title, transactions.amount, transactions.created_at from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and transactions.user_id = '.$user->id.' and (transactions.created_at between "'.$date1.'" and "'.$date2.'" ) group by security_jobs.id');
+			}else if($user->admin == 2){
+				$jobs = DB::select('select distinct security_jobs.id, security_jobs.title, transactions.amount, transactions.created_at from security_jobs, job_applications, transactions where job_applications.job_id = security_jobs.id and transactions.job_id = security_jobs.id and is_hired = 1 and applied_by = '.$user->id.' and (transactions.created_at between "'.$date1.'" and "'.$date2.'" ) group by security_jobs.id');
+			}
+    	}else{
+    		$jobs = array();
+    	}
+    	return view('wallet', compact('jobs', 'wallet_data'));
+    }
+
+	public function invoice(Request $request, $id){
+		// return view('invoice-employer');
+		$user = auth()->user();
+		$user_id = auth()->user()->id;
+        $balance = '';
+        
+        $from = array();        
+        $from = $user;
+	    $from->date = Carbon::now();
+        $all_transactions = array();
+
+        if(!empty($user_id)) {
+            if($user->admin == 2){
+                $all_transactions = DB::select('select security_jobs.title, transactions.id, transactions.amount, transactions.created_at, security_jobs.number_of_freelancers, transactions.credit_payment_status as status from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and transactions.type = "job_fee" and security_jobs.id = '.$id);
+            }else if($user->admin == 0){
+                $credit = Transaction::select(DB::raw('SUM(amount) as total'))
+                    ->groupBy('user_id')
+                    // ->where('user_id', $user_id)
+                    ->where('job_id', $id)
+                    ->where('status', 1)
+                    ->where(function($query){
+                        $query->orWhere('credit_payment_status', 'paid')
+                            ->orWhere('credit_payment_status', 'funded');
+                    })
+                    ->where('debit_credit_type', 'credit')
+                    ->get()->first();
+                $total_credit = !empty($credit->total) ? ($credit->total) : 0;
+                $balance = $total_credit;
+
+                $all_transactions = DB::select('select transactions.title, transactions.id, transactions.created_at, transactions.amount, security_jobs.number_of_freelancers, transactions.credit_payment_status as status from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and security_jobs.id = '.$id);
+                $applied_by = JobApplication::select('applied_by')->where('job_id', $id)->get();
+                // dd($applied_by);
+                foreach ($all_transactions as $key => $transactions) {
+                    if($transactions->title == 'Job Fee'){
+                        $transactions->user_id = $applied_by;
+                    }
+                }
+                // dd($all_transactions);
+            }
+        }
+        if (!empty($all_transactions)) {
+	        if($user->admin == 2){
+	            if($request->has('download')){
+		            $pdf = PDF::loadView('invoice-freelancer', compact('all_transactions', 'balance', 'from', 'id'));
+		            return $pdf->download('invoice.pdf');
+		        }
+	        	return view('invoice-freelancer', compact('all_transactions', 'balance', 'from', 'id'));
+	        }
+	        else if($user->admin == 0){
+	        	if($request->has('download')){
+		            $pdf = PDF::loadView('invoice-employer', compact('all_transactions', 'balance', 'from', 'id'));
+		            return $pdf->download('invoice.pdf');
+		        }
+	        	return view('invoice-employer', compact('all_transactions', 'balance', 'from', 'id'));
+	        }
+        }
+
+        return view('invoice-freelancer', compact('all_transactions', 'balance', 'from', 'id'));
+	}
+
+    public function freelancerInvoice(Request $request){
+        $user_id = $request->user_id;
+        $id = $request->id;
+        $all_transactions = DB::select('select security_jobs.title, transactions.id, transactions.amount, transactions.created_at, security_jobs.number_of_freelancers, transactions.credit_payment_status as status from security_jobs, transactions where transactions.job_id = security_jobs.id and transactions.status = 1 and transactions.type = "job_fee" and security_jobs.id = '.$id);
+
+        $from = User::find($user_id);
+        $from->date = Carbon::now();
+
+        if($request->has('download')){
+                    $pdf = PDF::loadView('invoice-freelancer', compact('all_transactions', 'balance', 'from', 'id'));
+                    return $pdf->download('invoice.pdf');
+                }
+                return view('invoice-freelancer', compact('all_transactions', 'balance', 'from', 'id'));
+    }
 }
--- local/resources/views/wallet.blade.php
+++ local/resources/views/wallet.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-    
-
    @include('style')
-	
-
-
-
-
+	<style src="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></style>
+	<style src="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css"></style>
+	<style src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css"></style>
+	<style type="text/css">
+		button {
+			background: #00a651;
+			color: white;
+			padding-top: 10px;
+			margin: 0;
+			border: none;
+			border-radius: 5px;
+			padding-left: 20px;
+			padding-right: 20px;
+			height: 40px;
+		}
+	</style>
 </head>
 <body>
 
@@ -31,7 +39,13 @@
 			</ol>						
 			<h2 class="title">Wallet</h2>
 		</div>
-	
+		    <div class="banner-form banner-form-full job-list-form">
+                <form method="get" action="{{ url('/wallet/jobs/find') }}" id="formID">
+                    <input type="text" class="form-control" placeholder="Job search" name="keyword" value="{{old('keyword')}}">
+
+                    <button type="submit" class="btn btn-primary" value="Search">Search</button>
+                </form>
+            </div>
 	
 
 
@@ -44,32 +58,43 @@
 						<div class="description-info">
 							<h2>Wallet</h2>
 							
-							
-							<table class="table table-striped">
-								<!-- <thead>
-									<tr>
-										<th>Title</th>
-										<th>Debit/Credit</th>
-										<th>Amount</th>
-									</tr>
-								</thead> -->
-								<tbody>
-								@if($wallet_data['all_transactions']->count() >0)
-									@foreach($wallet_data['all_transactions'] as $trans)
-										<tr>
-											<td>{{ $trans->title }}</td>
-											<td>{{ $trans->debit_credit_type }}</td>
-											<td>£ {{ $trans->amount }}</td>
-										</tr>
-									@endforeach
-								
-									<!-- <tr >
-										<td>title</td>
-										<td>credit/debit</td>
-										<td>amount</td>
-									</tr> -->
-								@endif
-								</tbody>
+							<div class="row">
+								<form method="get" action="{{ url('/wallet/jobs/find') }}" id="filters">
+							        <div class="col-sm-12">
+							        	<label class="col-sm-2">Transaction Date:</label>
+							            <div class="col-sm-3">
+							                <input type="text" class="start_date date-picker form-control" name="start_date" placeholder="Start Date" required="true"  value="{{old('start_date')}}">
+							                <span class="text-danger error-span"></span>
+							            </div>
+							            <div class="col-sm-3">
+							                <input type="text" class="end_date date-picker form-control" name="end_date" placeholder="End Date" required="true"  value="{{old('end_date')}}">
+							                <span class="text-danger error-span"></span>
+							            </div>
+							            <div class="col-sm-1">
+							            	<button type="submit" value="GO" class="btn btn-primary">GO</button>
+							            </div>
+							        </div>
+								</form>
+							</div>
+							<table class="display nowrap table" id="table">
+							    <thead>
+							        <tr>
+							            <th>Reference Number</th>
+							            <th>Job Title</th>
+							            <th>Amount</th>
+							            <th>Transaction Date</th>
+							        </tr>
+							    </thead>
+							    <tbody>
+							    	@foreach($jobs as $job)
+							        <tr>
+							            <td>{{$job->id}}</td>
+							            <td><a href="{{url('wallet/invoice/'.$job->id)}}">{{$job->title}}</a></td>
+							            <td>{{$job->amount}}</td>
+							            <td>{{date('d/m/Y',strtotime($job->created_at))}}</td>
+							        </tr>
+							        @endforeach
+							    </tbody>
 							</table>
 						</div>
 					</div>
@@ -88,8 +113,6 @@
 									@endif
 								</p>
 							</li>
-							
-							
 						</ul>
 					</div>
 
@@ -145,5 +168,49 @@
 
 
       @include('footer')
+
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
+
+<script type="text/javascript">
+
+$(document).ready(function() {
+    var table = $('#table').DataTable( {
+        dom: 'Bfrtip',
+        searching: false,
+        sorting: true,
+        buttons: [
+            'csv', 'excel', 'pdf',
+        ],
+        page_length: 50,
+    } );
+
+    $('#table_paginate').css('display', 'none');
+
+    $('#table tbody').on( 'click', 'tr', function () {
+    	var data = table.row(this).data();
+    	window.location = "{{url('/wallet/invoice/')}}"+'/'+data[0];
+        
+    } );
+
+	var date = new Date();
+	
+	$('.date-picker').datepicker({
+	    format: 'mm/dd/yyyy',
+	    autoclose: true,
+	});
+
+
+
+
+} );
+</script>
 </body>
 </html>
