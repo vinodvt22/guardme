867d3610e43bdb3021af7cf7352df01216e0631e
maysoon dahdouh <maysoon.azzat@gmail.com>
2018-05-21 10:14:32 +0200

    change

--- local/app/Http/Controllers/Api/WalletController.php
+++ local/app/Http/Controllers/Api/WalletController.php
@@ -24,7 +24,7 @@ class WalletController extends Controller
 
         $jobDetails = Job::getMyJobs();
         $data = array() ;
-      //  $calc = array() ;
+        //  $calc = array() ;
        if(count($jobDetails) ==0 ){
            $data[] = [
                'message'=>'failed',
@@ -34,22 +34,25 @@ class WalletController extends Controller
            return response()
                ->json($data, 200);
        } else{
-           foreach($jobDetails as $list){
-               $calc = Job::calculateJobAmount($list->id);
-               $data[] = [
-                   'message'=>'success',[
-                       'id'=>$list->id ? $list->id: null,
-                       'title'=>$list->title ? $list->title : null ,
-                       'payment_date' => $list->getJobTransactions['created_at'] ? $list->getJobTransactions['created_at']: null,
+
+           
+        foreach($jobDetails as $list){
+            $calc = Job::calculateJobAmount($list->id);
+            $data[] = [
+                'message'=>'success',[
+                    'id'=>$list->id ,
+                    'title'=>$list->title ,
+//                    'payment_date' => $list->getJobTransactions['created_at'] ,
 //                     'vat' => $calc['vat_fee'] ,
 //                     'amount' => $calc['grand_total']
-                   ]
-               ];
+                ]
+            ];
            }
 
-           return response()
-               ->json($data, 200);
-       }
+
+            return response()
+                ->json($data, 200);
+        }
 
 
 
@@ -59,20 +62,20 @@ class WalletController extends Controller
     public function getJobTransactionDetails($id){
         $amount = Job::calculateJobAmount($id) ;
         $job = Transaction::with(['getTransactionJob'])
-                           ->where('job_id' , $id)
-                           ->get();
+            ->where('job_id' , $id)
+            ->get();
 
 //        $job= Transaction::where('job_id' , $id )->get();
         $data = array();
         foreach($job as $list){
             $data[] = ['title'=> $list->title ,
-                         'date_of_payment' =>$list->created_at ,
-                         'paypal_ref'=> $list->paypal_id ,
-                         'vat'=>  $amount['vat_fee'] ,
-                          'commission'=>  $amount['admin_fee']   ,
-                          'job_fee'=> $amount['basic_total']
+                'date_of_payment' =>$list->created_at ,
+                'paypal_ref'=> $list->paypal_id ,
+                'vat'=>  $amount['vat_fee'] ,
+                'commission'=>  $amount['admin_fee']   ,
+                'job_fee'=> $amount['basic_total']
 
-                        ];
+            ];
         }
 
 
